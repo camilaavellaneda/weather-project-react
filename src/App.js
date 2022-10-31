ff97eae6b6eea87a8cead2925ada467535d7e5d0
camilaavellaneda <camilaavellaneda0903@gmail.com>
2022-10-31 15:40:56 -0600

    Added HTML STructure + CSS + Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import "./App.css";
 import React from "react";
-import SecondaryWeatherInfo from "./SecondaryWeatherInfo";
 import MainWeatherInfo from "./MainWeatherInfo";
-import WeatherSearchCityInput from "./WeatherSearchCityInput"
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <SecondaryWeatherInfo />
       <MainWeatherInfo />
-      <WeatherSearchCityInput />
       <footer>
         This project was coded by Camila Avellaneda and is{" "}
         <a
           href="https://github.com/camilaavellaneda/weather-project-react"
+          target={"_blank"}
+          className="author"
           no-referrer
         >
-          open sourced on Github
+          open-sourced on Github
         </a>
       </footer>
     </div>
--- src/MainWeatherInfo.js
+++ src/MainWeatherInfo.js
@@ -1,16 +1,76 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function MainWeatherInfo() {
   return (
     <div className="MainWeatherInfo">
-      <div>Temperature</div>
-      <div>
-        <span>C°</span>
-        <span>F°</span>
+      <div id="weather-card" className="card">
+        <div id="row-top" className="card-body">
+          <div id="col-left">
+            <div id="numberdate">Date</div>
+            <div id="hourday">Time & Weekday</div>
+            <div>
+              <div id="wind">Wind:</div>
+            </div>
+            <div>
+              <div id="humidity">Humidity:</div>
+            </div>
+          </div>
+          <div id="col-center">
+            <span id="element-temperature">Temperature </span>
+            <span id="element-celsius">C°</span> | 
+            <span id="element-farenheit">  F°</span>
+            <div id="city">City</div>
+            <div id="country">Country</div>
+            <div id="state">Description</div>
+          </div>
+          <div>
+            <div id="weather-icon"></div>
+          </div>
+        </div>
+        <div id="row-middle">
+          <form id="search-form" className="mb-3">
+            <div>
+              <input
+                type="search"
+                placeholder="Type a city.."
+                className="form-control"
+                id="search-city-input"
+                autocomplete="off"
+              />
+            </div>
+            <div>
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+                id="button-addon2"
+              />
+            </div>
+          </form>
+        </div>
+        <div id="weather-forecast" className="row card-body">
+          <div className="row">
+            <div className="col-2">
+              <div
+                className="weather-forecast-date"
+                id="weather-forecast-date"
+              ></div>
+              <img src="" alt="" width="36" id="forecast-weather-icon" />
+              <div className="weather-forecast-temperatures">
+                <span
+                  className="weather-forecast-temperature-max"
+                  id="weather-forecast-temperature-max"
+                ></span>
+                <span
+                  className="weather-forecast-temperature-min"
+                  id="weather-forecast-temperature-min"
+                ></span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>City</div>
-      <div>Country</div>
-      <div>Description</div>
     </div>
   );
 }
