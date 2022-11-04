139bc9341190fce2ca74bb195671ef70cde5a379
camilaavellaneda <camilaavellaneda0903@gmail.com>
2022-11-03 19:22:26 -0600

    Fix Warnings

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import "./App.css";
 import React from "react";
 import MainWeatherInfo from "./MainWeatherInfo";
 import "bootstrap/dist/css/bootstrap.css";
-import axios from "axios";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <MainWeatherInfo defaultCity="Berlin"/>
+      <MainWeatherInfo defaultCity="Berlin" />
       <footer>
         This project was coded by Camila Avellaneda and is{" "}
         <a
           href="https://github.com/camilaavellaneda/weather-project-react"
           target={"_blank"}
           className="author"
-          no-referrer="true"
+          rel="noreferrer"
         >
           open-sourced on Github
         </a>
